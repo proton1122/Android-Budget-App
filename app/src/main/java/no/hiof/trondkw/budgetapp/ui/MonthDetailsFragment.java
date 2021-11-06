3f6d8c6c7ce2b2a7152189dd8f2d6ae08e8903af
proton1122 <69960669+proton1122@users.noreply.github.com>
2021-11-06 15:58:35 +0100

    add DataBinding and ViewModel to MonthDetailsFragment class, but not complete setup.

--- app/src/main/java/no/hiof/trondkw/budgetapp/ui/MonthDetailsFragment.java
+++ app/src/main/java/no/hiof/trondkw/budgetapp/ui/MonthDetailsFragment.java
@@ -3,23 +3,43 @@ package no.hiof.trondkw.budgetapp.ui;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import no.hiof.trondkw.budgetapp.R;
+import no.hiof.trondkw.budgetapp.databinding.FragmentAddExpenseBinding;
+import no.hiof.trondkw.budgetapp.databinding.FragmentMonthDetailsBinding;
+import no.hiof.trondkw.budgetapp.viewmodels.BudgetMonthViewModel;
 
 public class MonthDetailsFragment extends Fragment {
+
+    private BudgetMonthViewModel budgetMonthViewModel;
+    private FragmentMonthDetailsBinding binding;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        budgetMonthViewModel = new ViewModelProvider(requireActivity()).get(BudgetMonthViewModel.class);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_month_details, container, false);
+        binding = FragmentMonthDetailsBinding.inflate(inflater, container, false);
+
+        // set this after adding <data> to xml
+        // binding.setBudgetMonthViewModel(....)
+
+        // observer viewModel...
+        // budgetMonthViewModel.get(...).observer.....
+
+        return binding.getRoot();
     }
-}
+
+
+
+} // end MonthDetailsFragment class
