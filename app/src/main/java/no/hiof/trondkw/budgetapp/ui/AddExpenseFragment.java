ba32e20b946542eaeaac2abb1672181cd813240b
proton1122 <69960669+proton1122@users.noreply.github.com>
2021-11-11 23:49:23 +0100

    Setup DatePickerDialog in AddExpenseFragment and layout.

--- app/src/main/java/no/hiof/trondkw/budgetapp/ui/AddExpenseFragment.java
+++ app/src/main/java/no/hiof/trondkw/budgetapp/ui/AddExpenseFragment.java
@@ -1,20 +1,27 @@
 package no.hiof.trondkw.budgetapp.ui;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import no.hiof.trondkw.budgetapp.databinding.FragmentAddExpenseBinding;
 import no.hiof.trondkw.budgetapp.viewmodels.BudgetMonthViewModel;
 
-public class AddExpenseFragment extends Fragment {
+public class AddExpenseFragment extends Fragment implements DatePickerDialog.OnDateSetListener {
 
     private BudgetMonthViewModel budgetMonthViewModel;
     private FragmentAddExpenseBinding binding;
+
+    private int dayOfMonth;
+    private int month;
+    private int year;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,12 +31,16 @@ public class AddExpenseFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = FragmentAddExpenseBinding.inflate(inflater, container, false);
 
-        // set this after adding <data> to xml
-        // binding.setBudgetMonthViewModel(....)
+        // set view model in binding
+        binding.setBudgetMonthViewModel(budgetMonthViewModel);
+
+        // set onclick listener for date picker dialog
+        binding.selectDateView.setOnClickListener(view -> showDatePickerDialog());
+
 
         // observer viewModel...
         // budgetMonthViewModel.get(...).observer.....
@@ -37,12 +48,34 @@ public class AddExpenseFragment extends Fragment {
         return binding.getRoot();
     }
 
+
+    // opens the DatePickerDialog
+    private void showDatePickerDialog() {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(), this, year, month, dayOfMonth);
+        datePickerDialog.show();
+    }
+
+
+    // gets the input from the DatePickerDialog and displays it
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        this.year = year;
+        this.month = month;
+        this.dayOfMonth = dayOfMonth;
+        String date = "day/month/year: " + dayOfMonth + "/" + (month + 1) + "/" + year;
+        binding.selectDateView.setText(date);
+    }
+
+
+
+    // -------------------------------------------------------------
     // needed?
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
+    // -------------------------------------------------------------
 
 
 } // end AddExpenseFragment class
