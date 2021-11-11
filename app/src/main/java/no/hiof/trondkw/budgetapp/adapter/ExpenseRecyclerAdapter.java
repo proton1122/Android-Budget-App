511a581677f91c502ab0e7e77e4fc995550a0750
proton1122 <69960669+proton1122@users.noreply.github.com>
2021-11-11 18:01:06 +0100

    Add expense_list_item layout for a single expense in the recycler view.

--- app/src/main/java/no/hiof/trondkw/budgetapp/adapter/ExpenseRecyclerAdapter.java
+++ app/src/main/java/no/hiof/trondkw/budgetapp/adapter/ExpenseRecyclerAdapter.java
@@ -1,4 +1,28 @@
 package no.hiof.trondkw.budgetapp.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+
+import java.util.List;
+
+import no.hiof.trondkw.budgetapp.models.Expense;
+
 public class ExpenseRecyclerAdapter {
+
+    private List<Expense> expenseList;
+    private LayoutInflater inflater;
+
+
+    public ExpenseRecyclerAdapter() {
+
+    }
+
+    public ExpenseRecyclerAdapter(Context context, List<Expense> expenseList) {
+        inflater = LayoutInflater.from(context);
+        this.expenseList = expenseList;
+    }
+
+
+
+
 }
