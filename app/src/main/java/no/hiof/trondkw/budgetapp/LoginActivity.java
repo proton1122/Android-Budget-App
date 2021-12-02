4d03f5e2f323784fdcc54eebbc45312fce73aa4b
proton1122 <69960669+proton1122@users.noreply.github.com>
2021-12-02 22:31:29 +0100

    Create LoginFragment layout xml and RegisterFragment layout xml.

--- app/src/main/java/no/hiof/trondkw/budgetapp/LoginActivity.java
+++ app/src/main/java/no/hiof/trondkw/budgetapp/LoginActivity.java
@@ -1,11 +1,14 @@
 package no.hiof.trondkw.budgetapp;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
-import android.os.Bundle;
+import no.hiof.trondkw.budgetapp.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,12 +16,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        ActivityLoginBinding binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        binding.setLifecycleOwner(this);
         setContentView(R.layout.activity_login);
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_login);
+        Toolbar toolbar = binding.toolbar;
 
-
-
-
+        NavigationUI.setupWithNavController(toolbar, navController);
     }
+
+
 }
