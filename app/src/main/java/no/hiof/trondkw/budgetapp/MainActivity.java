755e237b5dda6db3b73396e0aec7d19b32ae0364
proton1122 <69960669+proton1122@users.noreply.github.com>
2021-12-13 22:19:34 +0100

    Cleanup MonthOverviewFragment:

--- app/src/main/java/no/hiof/trondkw/budgetapp/MainActivity.java
+++ app/src/main/java/no/hiof/trondkw/budgetapp/MainActivity.java
@@ -155,7 +155,4 @@ public class MainActivity extends AppCompatActivity implements IBudgetDialogList
     }
 
 
-
-
-
 } // end MainActivity class
--- app/src/main/java/no/hiof/trondkw/budgetapp/ui/MonthOverviewFragment.java
+++ app/src/main/java/no/hiof/trondkw/budgetapp/ui/MonthOverviewFragment.java
@@ -6,24 +6,17 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import java.time.LocalDate;
-
 import no.hiof.trondkw.budgetapp.R;
 import no.hiof.trondkw.budgetapp.databinding.FragmentMonthOverviewBinding;
-import no.hiof.trondkw.budgetapp.models.BudgetMonth;
-import no.hiof.trondkw.budgetapp.repositories.BudgetMonthRepository;
-import no.hiof.trondkw.budgetapp.repositories.OnGetDataListener;
 import no.hiof.trondkw.budgetapp.ui.dialogs.BudgetDialog;
 import no.hiof.trondkw.budgetapp.ui.dialogs.MonthYearPickerDialog;
 import no.hiof.trondkw.budgetapp.utils.Utilities;
@@ -31,12 +24,8 @@ import no.hiof.trondkw.budgetapp.viewmodels.BudgetMonthViewModel;
 
 public class MonthOverviewFragment extends Fragment {
 
-    private final String TAG = "MonthOverviewFragment";
-
-
     private BudgetMonthViewModel budgetMonthViewModel;
     private FragmentMonthOverviewBinding binding;
-    private BudgetMonthRepository repository;
     private Canvas canvas;
 
 
@@ -45,7 +34,6 @@ public class MonthOverviewFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
         // Get view model
-        repository = BudgetMonthRepository.getInstance();
         budgetMonthViewModel = new ViewModelProvider(requireActivity()).get(BudgetMonthViewModel.class);
 
     } // end onCreate
@@ -79,22 +67,18 @@ public class MonthOverviewFragment extends Fragment {
 
         // Observe viewModel
         budgetMonthViewModel.getCurrentBudgetMonth().observe(requireActivity(), budgetMonth -> {
-            Log.d(TAG, "Observer called on ViewModel.currentMonth");
-            System.out.println("Observer called on ViewModel.currentMonth in onViewCreated().");
 
             // ViewModel changed, update data binding
             binding.setCurrentMonth(budgetMonthViewModel);
 
-            // TODO: Check if something has actually changed before drawing graph again?
             // Draw new graph
             Thread thread = new Thread(this::drawGraph);
             thread.start();
-
         });
 
         drawGraph();
-
     } // end onViewCreated
+
 
     public void openEditBudgetDialog() {
         BudgetDialog budgetDialog = new BudgetDialog();
@@ -132,44 +116,43 @@ public class MonthOverviewFragment extends Fragment {
     // TODO: Move to separate class
     private void drawGraph() {
 
-        Log.d(TAG, "Entered drawGraph()");
+        RectF rectangle = new RectF(100, 100, 900, 900);
 
-            RectF rectangle = new RectF(100, 100, 900, 900);
+        float budget = budgetMonthViewModel.getBudget().floatValue();
+        float expenses = budgetMonthViewModel.getTotalExpenses().floatValue();
 
-            float budget = budgetMonthViewModel.getBudget().floatValue();
-            float expenses = budgetMonthViewModel.getTotalExpenses().floatValue();
+        // getResources().getColor(int color) is deprecated and requires theme now
+        Paint basePaint = new Paint();
+        basePaint.setColor(Color.parseColor("#27272f"));
+        //basePaint.setColor(getResources().getColor(R.color.background_dark_grey));
+        basePaint.setStyle(Paint.Style.STROKE);
+        basePaint.setStrokeWidth(30);
 
-            // getResources().getColor(int color) is deprecated and requires theme now
-            Paint basePaint = new Paint();
-            basePaint.setColor(Color.parseColor("#27272f"));
-            //basePaint.setColor(getResources().getColor(R.color.background_dark_grey));
-            basePaint.setStyle(Paint.Style.STROKE);
-            basePaint.setStrokeWidth(30);
+        Paint budgetPaint = new Paint();
+        budgetPaint.setStyle(Paint.Style.STROKE);
+        budgetPaint.setStrokeWidth(25);
 
-            Paint budgetPaint = new Paint();
-            budgetPaint.setStyle(Paint.Style.STROKE);
-            budgetPaint.setStrokeWidth(25);
+        if(budget == 0) {
+            budgetPaint.setColor(Color.parseColor("#27272f"));
+        }
+        else if (expenses >= budget) {
+            //budgetPaint.setColor(getResources().getColor(R.color.rally_orange));
+            budgetPaint.setColor(Color.parseColor("#ff6859"));
+        }
+        else {
+            //budgetPaint.setColor(getResources().getColor(R.color.rally_primary_green));
+            budgetPaint.setColor(Color.parseColor("#1eb980"));
+        }
 
-            if(budget == 0) {
-                budgetPaint.setColor(Color.parseColor("#27272f"));
-            }
-            else if (expenses >= budget) {
-                //budgetPaint.setColor(getResources().getColor(R.color.rally_orange));
-                budgetPaint.setColor(Color.parseColor("#ff6859"));
-            }
-            else {
-                //budgetPaint.setColor(getResources().getColor(R.color.rally_primary_green));
-                budgetPaint.setColor(Color.parseColor("#1eb980"));
-            }
+        int startPos = -90;
+        float percent = (expenses / budget) * 100;
+        float circle = (float)360;
+        float angle = (circle / 100) * percent;
 
-            int startPos = -90;
-            float percent = (expenses / budget) * 100;
-            float circle = (float)360;
-            float angle = (circle / 100) * percent;
-
-            canvas.drawArc(rectangle, startPos, 360, false, basePaint);
-            canvas.drawArc(rectangle, startPos, angle, false, budgetPaint);
+        canvas.drawArc(rectangle, startPos, 360, false, basePaint);
+        canvas.drawArc(rectangle, startPos, angle, false, budgetPaint);
 
     } // end drawGraph()
+
 
 } // end MonthOverviewFragment class
