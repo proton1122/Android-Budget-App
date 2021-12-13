a6ba709534d213d9f68dc79dd97af8e5b14ea10f
proton1122 <69960669+proton1122@users.noreply.github.com>
2021-12-13 22:17:21 +0100

    Cleanup MainActivity.

--- app/src/main/java/no/hiof/trondkw/budgetapp/MainActivity.java
+++ app/src/main/java/no/hiof/trondkw/budgetapp/MainActivity.java
@@ -2,7 +2,6 @@ package no.hiof.trondkw.budgetapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,10 +20,6 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.firebase.ui.auth.AuthUI;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.api.LogDescriptor;
-import com.google.firebase.auth.FirebaseAuth;
 
 import java.time.LocalDate;
 
@@ -38,8 +33,6 @@ import no.hiof.trondkw.budgetapp.viewmodels.BudgetMonthViewModel;
 
 public class MainActivity extends AppCompatActivity implements IBudgetDialogListener, IMonthYearPickerDialogListener {
 
-    private final String TAG = "MainActivity";
-
     private ActivityMainBinding binding;
     private BudgetMonthViewModel budgetMonthViewModel;
     private BudgetMonthRepository repository;
@@ -52,7 +45,6 @@ public class MainActivity extends AppCompatActivity implements IBudgetDialogList
         // Set view model and repository
         repository = BudgetMonthRepository.getInstance();
         budgetMonthViewModel = new ViewModelProvider(this).get(BudgetMonthViewModel.class);
-        //budgetMonthViewModel.setDummyMonth();
 
         // Set up data binding
         binding = ActivityMainBinding.inflate(getLayoutInflater());
@@ -61,7 +53,6 @@ public class MainActivity extends AppCompatActivity implements IBudgetDialogList
 
         // Set default month (this month)
         setDefaultMonth();
-
 
         // Set up navigation with appbar / bottom navigation / drawer
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
@@ -85,7 +76,12 @@ public class MainActivity extends AppCompatActivity implements IBudgetDialogList
     } // end onCreate
 
 
-
+    // Set current month to be displayed when app starts
+    private void setDefaultMonth() {
+        int year = LocalDate.now().getYear();
+        int month = LocalDate.now().getMonth().getValue();
+        loadMonth(year, month);
+    }
 
     // Send input from EditBudgetDialog to viewModel
     @Override
@@ -93,40 +89,21 @@ public class MainActivity extends AppCompatActivity implements IBudgetDialogList
         budgetMonthViewModel.setBudget(budgetInput);
     }
 
-    private void setDefaultMonth() {
-        Log.d(TAG, "setDefaultMonth()");
-        int year = LocalDate.now().getYear();
-        int month = LocalDate.now().getMonth().getValue();
-
-        loadMonth(year, month);
-    }
-
     // Send input from MonthYearPickerDialog to viewModel
     @Override
     public void loadMonth(int year, int month) {
-        Log.d(TAG, "loadMonth(): year: "  + year + " month: " + month);
-
-        // while loading set an empty budgetMonth...?
-
         binding.progressBar.setVisibility(View.VISIBLE);
 
         repository.getMonth(year, month, new OnGetDataListener() {
             @Override
             public void onSuccess(BudgetMonth budgetMonth) {
-                Log.d(TAG, "repository.getMonth.onSuccess()");
-                System.out.println("MainActivity.loadMonth() onSuccess...");
                 binding.progressBar.setVisibility(View.GONE);
-
-                System.out.println("MainActivity.loadMonth.onSuccess budgetMonth retrieved from DB: " + budgetMonth);
-
                 budgetMonthViewModel.setBudgetMonth(budgetMonth);
             }
 
             @Override
             public void onFailure(Exception exception) {
-                Log.d(TAG, "repository.getMonth.onFailure()");
                 binding.progressBar.setVisibility(View.GONE);
-                System.out.println("MainActivity.loadMonth() onFailure...");
                 Toast.makeText(getApplicationContext(), "Error loading from database", Toast.LENGTH_LONG).show();
             }
         });
