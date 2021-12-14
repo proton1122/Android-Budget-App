7294e3597888f08b54ae9dc524e7dc4cfa6d35de
proton1122 <69960669+proton1122@users.noreply.github.com>
2021-12-14 22:08:08 +0100

    Final cleanup

--- app/src/main/java/no/hiof/trondkw/budgetapp/ui/login/RegisterFragment.java
+++ app/src/main/java/no/hiof/trondkw/budgetapp/ui/login/RegisterFragment.java
@@ -2,34 +2,20 @@ package no.hiof.trondkw.budgetapp.ui.login;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.NavigationUI;
-
-import android.util.Log;
 import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import com.firebase.ui.auth.data.model.User;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.Locale;
-
-import no.hiof.trondkw.budgetapp.LoginActivity;
 import no.hiof.trondkw.budgetapp.MainActivity;
 import no.hiof.trondkw.budgetapp.R;
 import no.hiof.trondkw.budgetapp.databinding.FragmentRegisterBinding;
@@ -67,7 +53,6 @@ public class RegisterFragment extends Fragment {
 
             // Try to create new user
             FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
-            //FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
 
                 if(task.isSuccessful()) {
                     // User successfully registered
